8b6c26dc8a264bcb9db47af2550d9135840613bc
keveeen <105822487+keveeen@users.noreply.github.com>
2023-01-25 18:19:39 +0100

    commit tree bedac43e2962bd9bc8e334c646e596b0942757d0

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,38 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+    validate :title_content
+    
+    
+    def title_content
+
+
+        if title 
+        phrases = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+
+        
+
+         if phrases.none?{ |w| title.include?(w) }
+
+                errors.add(:title, "Does not contain clickbait")
+        end
+    end
+            
+
+    end
+
+        # %w{ant bear cat}.none? { |word| word.length == 5 } #=> true
+      
+
+
+    #    [1, 2, 3].each do |n|
+    #     text = "Current number is: #{n}"
+    #     puts text
+    #   end
+
+ 
 end
